d01082452b34a1c364f487fc94b67d72d4b14554
Daniel Vinitsky <danielvinisky@gmail.com>
2024-08-12 13:42:15 +0300

    Add working routes edit & details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
 import { StatisticPageComponent } from './pages/statistic-page/statistic-page.component';
 import { ContactEditComponent } from './components/contact-edit/contact-edit.component';
 import { ContactDetailsPageComponent } from './pages/contact-details-page/contact-details-page.component';
+import { contactResolver } from './resolvers/contact.resolver';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'contact', component: ContactPageComponent},
   { path: 'contact/edit', component: ContactEditComponent},
-  { path: 'contact/edit/:contactId', component: ContactEditComponent},
-  { path: 'contact/:contactId', component: ContactDetailsPageComponent},
+  { path: 'contact/edit/:contactId', component: ContactEditComponent,resolve: { contact: contactResolver } },
+  { path: 'contact/:contactId', component: ContactDetailsPageComponent,resolve: { contact: contactResolver } },
   {path: 'statistics', component: StatisticPageComponent},
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', redirectTo: '/home' },
--- src/app/pages/contact-details-page/contact-details-page.component.ts
+++ src/app/pages/contact-details-page/contact-details-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { Contact } from '../../models/contact.model';
 @Component({
   selector: 'contact-details-page',
   templateUrl: './contact-details-page.component.html',
   styleUrl: './contact-details-page.component.scss'
 })
 export class ContactDetailsPageComponent {
+  private route = inject(ActivatedRoute)
+  
 
+  contact$: Observable<Contact> = this.route.data.pipe(map(data => data['contact']))
 }
